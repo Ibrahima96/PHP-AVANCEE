63d112d6a12ae42102b600b14fcebc85b31f122f
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-03-15 11:50:48 +0000

    Session

--- cookie.php
+++ cookie.php
@@ -1,20 +1,11 @@
 <!DOCTYPE html>
-
-<?php  
-
-$nomCookie = "usuario";
-$userCookie="Birame Diagne";
-$setcookie = setcookie($nomCookie, $userCookie, time() + 3600);
-
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cookie Doc</title>
+    <title>Session Doc</title>
 </head>
 <body>
-    <?php $retVal = isset($userCookie) ? "bonjour {$userCookie}" : "nom defini" ;?>
-    <h1> <?php echo $retVal; ?> </h1>
+    
 </body>
 </html>
