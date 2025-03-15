e75c762ee2566d36d37e191d1845f46c1f53f4bf
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-03-15 12:42:12 +0000

    session accomplit

--- cookie.php
+++ cookie.php
@@ -2,9 +2,9 @@
 <html lang="en">
     <?php    
     
-    $_SESSION['name'] = 'John Doe';
-    $_SESSION['email'] = 'john@example.com';
-    
+    $_SESSION['name'] = 'Kamado Tanjiro';
+    $_SESSION['email'] = 'kamado@tanjiro.com';
+
     ?>
 <head>
     <meta charset="UTF-8">
@@ -12,6 +12,13 @@
     <title>Session Doc</title>
 </head>
 <body>
+    <h1>Session Data</h1>
+  <?php $myName =isset($_SESSION['name']) ? "Vous vous appellez {$_SESSION['name'] }" : "Votre nom please" ;   ?>
+    
+  <?php $myMail =isset($_SESSION['name']) ? "votre mail est {$_SESSION['email'] }" : "Saisir votre adress mail please" ; ?>
+    <p><?php echo $myName; ?></p>
+    <p><?php echo $myMail; ?></p>
+    <a href="cookie.php">Go to cookie</a>
     
 </body>
 </html>
