2654b71791962536bf3cde9eabba63bd96def9e1
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-03-15 13:18:07 +0000

    commit

--- cookie.php
+++ cookie.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-    <?php    
+<?php    
     session_start();
     $_SESSION['name'] = 'Kamado Tanjiro';
     $_SESSION['email'] = 'kamado@tanjiro.com';
     
 
     ?>
+<!DOCTYPE html>
+<html lang="en">
+   
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
